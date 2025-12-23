846d5d1d8c741a72c4b2d743da8d0336917fbdf8
c-poteat <nellp7@yahoo.com>
2025-12-22 23:12:44 -0500

    updated

--- plugins/scaffolder-backend-module-enhanced-http/src/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Scaffolder backend module: http:backstage:frontend-json
- */
-export { scaffolderModuleFrontendJsonHttp as default } from './module';
--- plugins/scaffolder-frontend-json-viewer/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { scaffolderFrontendJsonPlugin } from './plugin';
